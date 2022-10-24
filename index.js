71633f81d992c3e5ecd23c5828cec508f2dbf910
Bruno Silveira <bruno.silveira02@hotmail.com>
2022-10-24 14:21:57 +0200

    GetOne order

--- index.js
+++ index.js
@@ -66,4 +66,40 @@ app.get('/orders', (req, res) => {
     .catch(e => res.sendStatus(500))
 });
 
+app.get('/orders/:id', (req, res) => {
+    const { id } = req.params;
+    pool
+    .query('SELECT * FROM orders WHERE id=$1;', [id])
+    .then(data => res.json(data))
+    .catch(e => res.sendStatus(500))
+});
+
+// // POST / -> To create a new user 
+app.post('/', (req, res) => {
+    const { name } = req.body;
+    pool
+    .query('INSERT INTO users(name) values($1) RETURNING *;', [name])
+    .then(data => res.status(201).json(data))
+    .catch(e => res.sendStatus(500))
+});
+
+// // PUT /:id  :  To edit one user (with the id) 
+app.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    pool
+    .query('UPDATE users SET name=$1 WHERE id=$2 RETURNING *;', [name, id])
+    .then(data => res.status(201).json(data))
+    .catch(e => res.sendStatus(500))
+});
+
+// // DELETE  /:id : To delete one user (with the id)
+app.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    pool
+    .query('DELETE users WHERE id=$1;', [id])
+    .then(data => res.status(201).json(data))
+    .catch(e => res.sendStatus(500))
+});
+
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
