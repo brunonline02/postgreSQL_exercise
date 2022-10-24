28c25be97f196407eecd17f4830e1fcfcee3bdd4
Bruno Silveira <bruno.silveira02@hotmail.com>
2022-10-24 14:17:20 +0200

    GetAll orders

--- index.js
+++ index.js
@@ -14,43 +14,55 @@ const pool = new Pool ({
     password: process.env.DB_PASSWORD,
 }) 
 
-app.get('/', (req, res) => {
+// GET  /  : To get all the users 
+// app.get('/', (req, res) => {
+//     pool
+//     .query('SELECT * FROM users;')
+//     .then(data => res.json(data))
+//     .catch(e => res.sendStatus(500))
+// });
+
+// // GET  /:id :  To get one user (with the id) 
+// app.get('/:id', (req, res) => {
+//     const { id } = req.params;
+//     pool
+//     .query('SELECT * FROM users WHERE id=$1;', [id])
+//     .then(data => res.json(data))
+//     .catch(e => res.sendStatus(500))
+// });
+
+// // POST / -> To create a new user 
+// app.post('/', (req, res) => {
+//     const { name } = req.body;
+//     pool
+//     .query('INSERT INTO users(name) values($1) RETURNING *;', [name])
+//     .then(data => res.status(201).json(data))
+//     .catch(e => res.sendStatus(500))
+// });
+
+// // PUT /:id  :  To edit one user (with the id) 
+// app.put('/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { name } = req.body;
+//     pool
+//     .query('UPDATE users SET name=$1 WHERE id=$2 RETURNING *;', [name, id])
+//     .then(data => res.status(201).json(data))
+//     .catch(e => res.sendStatus(500))
+// });
+
+// // DELETE  /:id : To delete one user (with the id)
+// app.delete('/:id', (req, res) => {
+//     const { id } = req.params;
+//     pool
+//     .query('DELETE users WHERE id=$1;', [id])
+//     .then(data => res.status(201).json(data))
+//     .catch(e => res.sendStatus(500))
+// });
+
+app.get('/orders', (req, res) => {
     pool
-    .query('SELECT * FROM users;')
+    .query('SELECT * FROM orders;')
     .then(data => res.json(data))
-    .catch(e => res.sendStatus(500))
-});
-
-app.get('/:id', (req, res) => {
-    const { id } = req.params;
-    pool
-    .query('SELECT * FROM users WHERE id=$1;', [id])
-    .then(data => res.json(data))
-    .catch(e => res.sendStatus(500))
-});
-
-app.post('/', (req, res) => {
-    const { name } = req.body;
-    pool
-    .query('INSERT INTO users(name) values($1) RETURNING *;', [name])
-    .then(data => res.status(201).json(data))
-    .catch(e => res.sendStatus(500))
-});
-
-app.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    pool
-    .query('UPDATE users SET name=$1 WHERE id=$2 RETURNING *;', [name, id])
-    .then(data => res.status(201).json(data))
-    .catch(e => res.sendStatus(500))
-});
-
-app.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    pool
-    .query('DELETE users WHERE id=$1;', [id])
-    .then(data => res.status(201).json(data))
     .catch(e => res.sendStatus(500))
 });
 
